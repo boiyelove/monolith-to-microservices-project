c7bd38f3613d1efd3374f7ca35144adbf9112740
Damilola Boiyelove <daahrmmieboiye@gmail.com>
2022-09-09 22:48:16 +0100

    updated deployment file after pods running

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://adc68315fa4ab4a168b5ea01d298c856-1268835556.us-east-1.elb.amazonaws.com:8080/api/v0'  // -- reverseproxy
+  apiHost: 'http://a54a02e641e284a0594b2e855d7a2870-472853427.us-east-1.elb.amazonaws.com:8080/api/v0'  // -- reverseproxy
   // apiHost: 'http://localhost:8080/api/v0'
 };
 
